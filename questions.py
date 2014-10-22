74da8d4ccf7e37e7bcd11ef1dd0e27ca377ac10f
Peter Xu <peterxu30@gmail.com>
2014-10-22 02:04:19 -0700

    questions file

---
+++ questions.py
@@ -0,0 +1,2 @@
+Questions = {1:['Who was the first president of the United States?', 'George Washington'], 2:['Who is the current president of the United States?', 'Barack Obama'], 
+3:['On the Mexican flag, the eagle is perched upon what kind of plant?', 'Cactus']}
