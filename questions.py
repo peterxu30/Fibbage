d2941a20a87c9514ec63b58ebf934d27ec99bb61
peterxu30 <peterxu30@gmail.com>
2015-02-24 15:08:25 -0800

    Added documentation for questions.py

--- questions.py
+++ questions.py
@@ -1,2 +1,4 @@
+# Questions are stored in a dictionary. The key is the question number and the corresponding question-answer pair is stored as a list. 
+
 Questions = {1:['Who was the first president of the United States?', 'George Washington'], 2:['Who is the current president of the United States?', 'Barack Obama'], 
 3:['On the Mexican flag, the eagle is perched upon what kind of plant?', 'Cactus']}
